20c7ea87818f5b69759a2f9c3023c9aa89a85c38
Fair-Exchange <Fair-Exchange@protonmail.com>
2018-02-18 01:04:11 -0700

    komodo bitcore patched for insight block explorer

---
+++ depends.backup/packages/packages.mk
@@ -0,0 +1,13 @@
+zcash_packages := libsnark libgmp libsodium
+
+ifeq ($(build_os),darwin)
+packages := boost openssl $(zcash_packages)
+else
+packages := boost openssl $(zcash_packages) googletest googlemock
+endif
+
+native_packages := native_ccache
+
+wallet_packages=bdb
+
+upnp_packages=miniupnpc
